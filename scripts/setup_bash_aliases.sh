8056c4b3c29c1f6f7a188db586c6946414d30394
Wei Wang <wei_company@126.com>
2019-06-19 11:06:03 +0800

    new setup script: add aliases to ~/.bash_aliases

---
+++ scripts/setup_bash_aliases.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e  # exit on first error
+
+SCRIPT_DIR=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
+
+toolsSetup() {
+    # git_update_batch alias
+    if (grep -q "git_update_batch" ~/.bash_aliases); then
+        echo "Found bashrc entry: git_update_batch=$SCRIPT_DIR/tools/git_update_batch.sh"
+    else
+        echo "alias git_update_batch='${SCRIPT_DIR}/tools/git_update_batch.sh'" >> ~/.bash_aliases
+    fi
+    
+    # find_git_repo alias
+    if (grep -q "find_git_repo" ~/.bash_aliases); then
+        echo "Found bashrc entry: find_git_repo=$SCRIPT_DIR/tools/find_git_repo.sh"
+    else
+        echo "alias find_git_repo='${SCRIPT_DIR}/tools/find_git_repo.sh'" >> ~/.bash_aliases
+    fi
+    
+    # update
+    source ~/.bashrc
+}
+
+main() {
+    toolsSetup
+}
+
+main
