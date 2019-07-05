0568f828f8c351db3ff2b2e610e5187749492745
Wei Wang <wei_company@126.com>
2019-07-05 11:42:03 +0800

    update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,20 +2,21 @@
 set -e  # exit on first error
 
 SCRIPT_DIR=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
+BIN_DIR=$HOME/bin
 
-toolsSetup() {
+setupAliases() {
     # git_update_batch alias
-    if (grep -q "git_update_batch" ~/.bashrc); then
+    if (grep -q "git_update_batch" ~/.bash_aliases); then
         echo "Found bashrc entry: git_update_batch=$SCRIPT_DIR/tools/git_update_batch.sh"
     else
-        echo "alias git_update_batch='${SCRIPT_DIR}/tools/git_update_batch.sh'" >> ~/.bashrc
+        echo "alias git_update_batch='${SCRIPT_DIR}/tools/git_update_batch.sh'" >> ~/.bash_aliases
     fi
     
     # find_git_repo alias
-    if (grep -q "find_git_repo" ~/.bashrc); then
+    if (grep -q "find_git_repo" ~/.bash_aliases); then
         echo "Found bashrc entry: find_git_repo=$SCRIPT_DIR/tools/find_git_repo.sh"
     else
-        echo "alias find_git_repo='${SCRIPT_DIR}/tools/find_git_repo.sh'" >> ~/.bashrc
+        echo "alias find_git_repo='${SCRIPT_DIR}/tools/find_git_repo.sh'" >> ~/.bash_aliases
     fi
     
     # update
@@ -23,7 +24,7 @@ toolsSetup() {
 }
 
 main() {
-    toolsSetup
+    setupAliases
 }
 
 main
